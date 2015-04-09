c6381fa57a31240de796b639e99a113f51d4c79b
Erica Cha & Bojana Skarich <student@epicodus.com>
2015-04-09 11:41:19 -0700

    Adjust for loop to stop looping when it gets to the last array element in array_ones.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
 var scrabbleWord = function(input_word) {
-    if (input_word === "a") {
-        return 1;
 
-    }
+    var word_score = 0;
+
+    var array_ones = ["a", "e", "i", "o", "u", "l", "n", "r", "s", "t"];
+
+    var array_split_word = input_word.split('');
+
+        for (var i = 0; i < array_ones.length; i++) {
+            if (array_split_word[i] === array_ones[i]) {
+
+                word_score += 1;
+            
+            }
+        }
+    return word_score;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,11 @@ describe('scrabbleWord', function() {
     it("returns a score of 1 if the input letter is 'a'", function() {
         expect(scrabbleWord('a')).to.equal(1);
     });
+
+    it("returns a score of 2 if the input letters are 'ae'", function() {
+        expect(scrabbleWord("ae")).to.equal(2);
+    });
+    it("returns a score of 3 if the input letters are all in the array_ones", function() {
+        expect(scrabbleWord("aei")).to.equal(3);
+    });
 });
