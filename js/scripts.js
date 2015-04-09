36251257164fb3db10818b941572284f35dba335
Erica Cha & Bojana Skarich <student@epicodus.com>
2015-04-09 09:54:54 -0700

    Add initial test and method. Add initial files.

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var scrabbleWord = function(input_word) {
+    if (input_word === "a") {
+        return 1;
+
+    }
+};
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe('scrabbleWord', function() {
+    it("returns a score of 1 if the input letter is 'a'", function() {
+        expect(scrabbleWord('a')).to.equal(1);
+    });
+});
